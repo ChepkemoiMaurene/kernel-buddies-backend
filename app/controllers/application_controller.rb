01c31211bc9fce444395d4e27fd3f77c3aebd4f1
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-11-11 01:12:34 +0300

    added the route and converted the response to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/pets' do
+    pets=Pet.all
+    pets.to_json 
   end
 end
